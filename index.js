04922d3d8d8f78ffa2f131c01cc63a8c42c8b751
Rolexex <105866938+Rolexex@users.noreply.github.com>
2024-10-04 18:29:41 +0300

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎮 Playing RegalV2", "Regal Roleplay"];
+const statusMessages = ["V2 S00N", "Regal Roleplay"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
